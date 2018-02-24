da631ba40e23b178bc354eec4a28c2ae07ae8199
h-shan <howardshan11@gmail.com>
2018-02-24 15:09:13 -0600

    Differentiate between subject and subjectless messages

--- chat_util/sentence_gen.py
+++ chat_util/sentence_gen.py
@@ -11,23 +11,43 @@ neg_file = open('message_templates/neg.txt', 'r')
 neg_arr = neg_file.read().splitlines()
 neg_file.close()
 
+pos_subj_file = open('message_templates/pos_subj.txt', 'r')
+pos_subj_arr = pos_subj_file.read().splitlines()
+pos_subj_file.close()
+
+neg_subj_file = open('message_templates/neg_subj.txt', 'r')
+neg_subj_arr = neg_subj_file.read().splitlines()
+neg_subj_file.close()
+
 ques_file = open('message_templates/questions.txt', 'r')
 ques_arr = ques_file.read().splitlines()
 ques_file.close()
+
+ques_subj_file = open('message_templates/questions.txt', 'r')
+ques_subj_arr = ques_subj_file.read().splitlines()
+ques_subj_file.close()
 
 def respond(cur_senti, prev_senti, subject):
     p_pos_senti = 1.5 - cur_senti - prev_senti
     if random.uniform(0, 1) <= p_pos_senti:
         return generate_sentence(subject, cur_senti)
     else:
-        ques_temp = random.choice(ques_arr)
-        return ques_temp.replace("{{}}", subject)
+        return generate_question(subject)
+
+def generate_question(subject):
+    if subject:
+        return random.choice(ques_subj_arr).format(subject)
+    else:
+        return random.choice(ques_arr)
 
 def generate_sentence(subject, cur_senti):
     if cur_senti < 0.5:
-        pos_temp = random.choice(pos_arr)
-        return pos_temp.replace("{{}}", subject)
+        if subject:
+            return random.choice(pos_subj_arr).format(subject)
+        else:
+            return random.choice(pos_arr)
     else:
-        neg_temp = random.choice(neg_arr)
-        return neg_temp.replace("{{}}", subject)
-
+        if subject:
+            return random.choice(neg_subj_arr).format(subject)
+        else:
+            return random.choice(neg_arr)
