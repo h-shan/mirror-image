49dafa489bdbf2dfcbb474947b1c6c2d8021cffc
h-shan <howardshan11@gmail.com>
2018-02-23 23:49:18 -0600

    Remove api keys from source code

--- TwitterMirrorImage.py
+++ TwitterMirrorImage.py
@@ -1,15 +1,10 @@
 import tweepy
+import config
 
 def parseTweets(userName):
-
-	CONSUMER_KEY = 'SALYJG3o3YtK1zQvBadKTRQqf'
-	CONSUMER_SECRET = 'fxAi3hfJtKWwuzkLNhp07CTWmo3d4iAwcWc4uSdtC9VwJtSXvK'
-	ACCESS_TOKEN = '842097932-85kJcDamVo8CLCAuGJ1yr3bdo5O9GZ5Nmh7rCypQ'
-	ACCESS_SECRET = 'BGrnvIoos3imGaIZM8tnHGHSrxHGADXKZnarX8bG7P7a2'
-
 	#authorizes the twitter account to be able to access tweets
-	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+	auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+	auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
 	api = tweepy.API(auth)
 
 	tweetsFromUser = []
