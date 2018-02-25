31b14f9f125f4cb1e452f0e9beca9e6f9fa09b87
h-shan <howardshan11@gmail.com>
2018-02-24 21:06:39 -0600

    Remove verification

--- app.py
+++ app.py
@@ -16,16 +16,16 @@ prev_senti = 0.5
 def test():
     return 'TEST SUCCESS', 200
 
-@app.route('/', methods=['GET'])
-def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
+# @app.route('/', methods=['GET'])
+# def verify():
+#     # when the endpoint is registered as a webhook, it must echo back
+#     # the 'hub.challenge' value it receives in the query arguments
+#     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+#         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+#             return "Verification token mismatch", 403
+#         return request.args["hub.challenge"], 200
 
-    return "Hello world", 200
+#     return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
