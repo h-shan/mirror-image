fb8921af210179d348af403037167257415e0b5d
h-shan <howardshan11@gmail.com>
2018-02-24 21:02:43 -0600

    Add test endpoint to api

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-import os
+from datetime import datetime
 import sys
 import json
-import requests
-from datetime import datetime
-from flask import Flask, request
+import os
 
-from chat_util import text_analytics, response, luis_request
+from flask import Flask, request
+import requests
+
+from chat_util import text_analytics, response
 
 app = Flask(__name__)
 
 prev_senti = 0.5
+
+@app.route('/test', methods=['GET'])
+def test():
+    return 'TEST SUCCESS', 200
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -31,34 +36,8 @@ def webhook():
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    '''if data["object"] == "page":
-
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-
-                if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                    cur_senti, keyword = text_analytics.analyze(message_text)
-                    #luis_res = luis_request.request_luis(message_text)
-                    
-                    global prev_senti
-                    send_message(sender_id, response.respond(cur_senti, prev_senti, keyword, message_text))
-                    prev_senti = cur_senti
-
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
-
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
-
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-    '''
     response_message = ''
-    if 'message' in data:
+    if data and 'message' in data:
         message_text = data['message']  # the message's text
         cur_senti, keyword = text_analytics.analyze(message_text)
         
